3e225ded148cefab52b2920b12ff5305699d156a
yahsuanlinus <savannahwang75@gmail.com>
2022-01-23 03:27:09 -0500

    finished

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,42 @@
 // YOUR CODE HERE
+
+class $ {
+	constructor(str) {
+		if(str.startsWith('.')) {
+			this.elements = [...document.getElementsByClassName(str.slice(1))]
+		} else if (str.startsWith('#')) {
+			this.elements = [document.getElementById(str.slice(1))]
+		} else {
+			this.elements = [...document.getElementsByTagName(str)]
+		}
+	}
+
+	hide() {
+		this.elements.map(ele => ele.style.display = 'none')
+		return this
+	}
+
+	show() {
+		this.elements.map(ele => ele.style.display = 'inherit') 
+		return this
+	}
+
+	addClassName(arg) {
+		this.elements.map(ele => ele.classList.add(arg))
+		return this
+	}
+
+	removeClassName(arg) { this.elements.map(ele => ele.classList.remove(arg))
+		return this
+	}
+
+	text(txt) { this.elements.map(ele => ele.innerHTML = txt)
+		return this
+	}
+
+	addChild(tag) { this.elements.map(ele => { 
+		const x = document.createElement(tag) 
+		ele.appendChild(x)
+		})
+	}
+}
